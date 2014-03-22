d148f0431afa2ee93b5a6fc89f0a149bd94bfc49
root <kilexst@gmail.com>
2014-03-22 11:09:59 +0400

    add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo cp noolitepc /usr/bin
+sudo cp nooliterx /usr/bin
+sudo cp nooliterxcfg /usr/bin
+sudo chown root /usr/bin/noolitepc
+sudo chown root /usr/bin/nooliterx
+sudo chown root /usr/bin/nooliterxcfg
